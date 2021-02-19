9367f9ccedbb7d43901731926df7c3e36dfd5582
Ryder Mackay <ryder.mackay@gmail.com>
2021-02-19 13:05:24 -0500

    Initial Commit

---
+++ Shared/MatchingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingGameApp.swift
+//  Shared
+//
+//  Created by Ryder Mackay on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct MatchingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
